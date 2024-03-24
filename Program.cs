dbef2d38af062d966222b88eefa3fb3215f87288
Lolkek4ik <158052830+Lolkek4ik@users.noreply.github.com>
2024-03-24 12:55:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,22 @@ namespace Ldl3
 {
     internal class Program
     {
-        static git Git = new git();
-        static void Main()
+        
+         static git gitInstance = new git();
+         static GitBackgroundThread backgroundThread = new GitBackgroundThread(gitInstance, 5000);
+        static public void Main()
         {
             string input;
+            
 
         start:;
 
             Console.Write(">");
             input = Console.ReadLine();
-            if (input.StartsWith("info ")) { Git.info(input); Console.WriteLine("\n"); goto start; }
-            else if (input == "commit") { Git.commit(); Console.WriteLine("\n"); goto start; }
-            else if (input == "status") { Git.status(); Console.WriteLine("\n"); goto start; }
-            else if (input == "end") { return; }
+            if (input.StartsWith("info ")) { gitInstance.info(input); Console.WriteLine("\n"); goto start; }
+            else if (input == "commit") { gitInstance.commit(); backgroundThread.Start(); Console.WriteLine("\n"); goto start; }
+            else if (input == "status") { gitInstance.status(); Console.WriteLine("\n"); goto start; }
+            else if (input == "end") { backgroundThread.Stop(); return; }
             else { Console.WriteLine($"Operation {input} is not a valid operation"); goto start; }
 
 
